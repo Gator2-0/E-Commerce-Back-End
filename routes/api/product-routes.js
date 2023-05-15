e3bd1c78e907a83b52db6d4197193ba6862e1763
Jeremy <jejedech.63@gmail.com>
2023-05-15 18:22:04 +1000

    fixing route delete product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,14 +19,13 @@ router.get('/', async (req, res) => {
 
 // get one product
 router.get('/:id', async (req, res) => {
-  console.log("get product by id");
+  
   try {
     const productData = await Product.findByPk(req.params.id, {
       include: [{model: Tag, through: ProductTag, as: 'tagIds'},
                 {model: Category}]
     });
-    console.log("productData by id");
-    console.log(productData);
+    
     if (!productData) {
       res.status(404).json({ message: 'No product found with this id!'});
       return;
@@ -114,7 +113,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const productData = await Category.destroy({
+    const productData = await Product.destroy({
       where: {
         id: req.params.id,
       },
